1000e5b9701f2bd3454ac8539ee957e5432fbc0d
Michael Eatherly <meatherly@gmail.com>
2015-05-13 11:43:05 -0500

    Editing the application layout

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Phitter.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Phitter"
   end
 end
